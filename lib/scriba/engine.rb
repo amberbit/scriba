ffaf4c08eb1bba8a5f9150dc8aa5a8bd94986b83
Hubert Łępicki <hubert.lepicki@amberbit.com>
2012-05-30 12:25:30 +0200

    prefer rspec over test/unit

--- lib/scriba/engine.rb
+++ lib/scriba/engine.rb
@@ -1,5 +1,8 @@
 module Scriba
   class Engine < ::Rails::Engine
     isolate_namespace Scriba
+    config.generators do |g|
+      g.test_framework :rspec, :view_specs => false
+    end
   end
 end
--- test/scriba_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class ScribaTest < ActiveSupport::TestCase
-  test "truth" do
-    assert_kind_of Module, Scriba
-  end
-end
